4c74e4ce25e2e01972a4f02d3172e7c8874e2190
jswanson806@icloud.com <jake@DESKTOP-0S9V7C7.localdomain>
2022-10-31 07:04:25 -0500

    refactored code to be object-oriented

--- static/app.js
+++ static/app.js
@@ -1,5 +1,4 @@
-let gameOver = false;
-let score = 0;
+
 /** Checks validity of word and updates game score
  * 
  * Return message based on response from the server -
@@ -9,98 +8,113 @@ let score = 0;
  * 
  * display the response to the user & points from word
  */
+class BoggleGame{
+    constructor(boardId, sec = 60) {
+    this.sec = sec;
+    this.displayTimer();
 
-async function handleSubmit(evt) {
-    evt.preventDefault();
-    const $word = $('#form-input');
-    let word = $word.val();
-    let points = word.length;
-    // stop execution if no word is entered
-    if (!word) return;
-
-    try {
-        const response = await axios.get("/validate-word", {params: {word: word}});
-        const result = response.data.result;
-        if (result === "not-word"){
-            showMessage(`${word} is not a valid word.`, 'err')
-        } else if (result == "not-on-board") {
-            showMessage(`${word} is not on the board.`, 'err')
-        } else {
-            showMessage(`Added: ${word} +${points}`, 'ok')
-            //update score with points from word
-            score += points;
-            //update score in DOM
-            updateScore(score);
-        }
-
-    //catch errors
-    } catch (err) {
-        console.error("handleSubmit failed", err);
-    }
-    //clear input and focus
-    $word.val("").focus();
-}
-
-//listener for the input form submission
-$("#word-form").on('submit', handleSubmit);
-
-//update DOM with message to user
-function showMessage(msg) {
-    $(".msg").html("").append(`<p>${msg}</p>`);
-}
-
-//update DOM with current score
-function updateScore(score){
-    $(".score").html(`${score}`);
-}
-
-//handles game timer
-function startTimer() {
-    let sec = 10;
+    this.score = 0;
+    this.words = new Set();
+    this.board = $("#" + boardId);
     
-        setInterval(() => {
-            //if game ends, stop the timer
-            if(!gameOver) {
-            $("#timer").html(`Time Remaining: ${sec}`);
-            sec--;
-            //if timer reaches 0, call endGame function
-            if (sec == -1) {
-                endGame();
-            }
-        }
-        }, 1000);
-}
+    this.timer = setInterval(this.countDown.bind(this), 1000);
 
-function startGame() {
-    startTimer();
-}
-
-function hideComponents() {
-    $('.display-area').hide()
-    $('.form').hide()
-    $('.score-display').hide()
-    $('.timer').hide()
-}
-
-//remove input form, display 'game over' and final score
-async function endGame() {
-    gameOver = true;
-    hideComponents();
-    showMessage(`Game Over! Final Score: ${score}`);
-
-    // post request with game score
-    try {
-        const response = await axios.post("/stats", {score: score});
-        if(response.data.newRecord){
-            showMessage(`Game Over! New High Score is ${score}!`);
-        }
-    } catch (err) {
-        console.error("endGame failed", err);
+    //listener for the input form submission
+    $("#word-form", this.board).on('submit', this.handleSubmit.bind(this));
+    
     }
-}
 
-//start the game timer after the DOM loads
-document.addEventListener('DOMContentLoaded', () => {
-    startGame();
-})
+    async handleSubmit(evt) {
+        evt.preventDefault();
+        const $word = $('#form-input', this.board);
+        let word = $word.val();
+        let points = word.length;
+        // stop execution if no word is entered
+        if (!word) return;
+
+        if(this.words.has(word)) {
+            this.showMessage(`Already found '${word}'`, 'err');
+            return;
+        }
+
+        try {
+            const response = await axios.get("/validate-word", {params: {word: word}});
+            const result = response.data.result;
+            if (result === "not-word"){
+                this.showMessage(`${word} is not a valid word.`, 'err')
+            } else if (result == "not-on-board") {
+                this.showMessage(`${word} is not on the board.`, 'err')
+            } else {
+                
+                //update score with points from word
+                this.score += points;
+                this.words.add(word);
+                //update score in DOM
+                this.updateScore();
+                this.showMessage(`Added: ${word} +${points}`, 'ok')
+            }
+
+        //catch errors
+        } catch (err) {
+            console.error("handleSubmit failed", err);
+        }
+        //clear input and focus
+        $word.val("").focus();
+    }
+
+
+    //update DOM with message to user
+    showMessage(msg, cls) {
+        $(".msg", this.board)
+        .text(msg)
+        .removeClass()
+        .addClass(`msg ${cls}`);
+    }
+
+    //update DOM with current score
+    updateScore(){
+        $(".score", this.board).html(`${this.score}`);
+    }
+
+    //handles game timer
+    displayTimer() {    
+        $("#timer", this.board).text(`Time Remaining: ${this.sec}`);
+            
+    }
+
+    async countDown() {
+        this.sec -= 1;
+        this.displayTimer();
+
+        if (this.sec === 0) {
+            clearInterval(this.timer);
+            await this.endGame();
+        }
+    }
+
+    
+
+    hideComponents() {
+        $('.display-area', this.board).hide()
+        $('.form', this.board).hide()
+        $('.score-display', this.board).hide()
+        $('.timer', this.board).hide()
+    }
+
+    //remove input form, display 'game over' and final score
+    async endGame() {
+        this.hideComponents();
+        this.showMessage(`Game Over! Final Score: ${this.score}`);
+
+        // post request with game score
+        const response = await axios.post("/stats", {score: 
+                                                    this.score});
+
+        if(response.data.newRecord){
+            this.showMessage(`Game Over! New High Score is ${this.score}!`);
+        }
+    }
+
+
+}
 
